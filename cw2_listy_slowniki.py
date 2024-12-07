6442668e6584f49b8282c123e82202dcf429ffc7
magu_da <cokolwiekkkk@blabla.com>
2024-12-07 14:16:19 +0100

    dodaj cwiczenie na listach i slownikach

---
+++ cw2_listy_slowniki.py
@@ -0,0 +1,9 @@
+# lista owocow
+owocki = ["ananas", "kiwi", "poziomka"]
+
+#slownik z cenami
+ceny = {"ananas": 12, "kiwi": 2, "poziomka":"strasznie duzo "}
+
+# wyswietl ceny owockow
+ulubiony = owocki[0]  # ananas
+print(f"Moj ulubiony owoc to {ulubiony} i kosztuje jakies {ceny[ulubiony]}zl.")

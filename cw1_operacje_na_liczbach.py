3aa16161420fb3768ef8d7cbc50c9c228f3f0502
magu_da <cokolwiekkkk@blabla.com>
2024-12-07 13:47:50 +0100

    dodaj zadanie 1

---
+++ cw1_operacje_na_liczbach.py
@@ -0,0 +1,10 @@
+# dane od uzytkownika
+number1 = float(input("Podaj pierwsza liczbe: "))
+number2 = float(input("Podaj druga liczbe: "))
+
+# operacje matematyczne
+add = number1 + number2
+potegowanie = number1 ** number2
+
+print(f"Suma twoich liczb: {number1} oraz {number2} to {add}")
+print(f"Natomiast {number1} do potegi {number2} to {potegowanie}")
